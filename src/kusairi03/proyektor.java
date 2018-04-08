0358dca58e1524be163411858ab1e7f2a08ac7a9
kusairi <riskadelina96@gmail.com>
2018-04-08 21:27:29 +0800

    buat method cetakinfo()

--- src/kusairi03/proyektor.java
+++ src/kusairi03/proyektor.java
@@ -4,4 +4,12 @@ public class proyektor {
    private String jenis;
    private String merk;
    private double harga;
+   
+void cetakInfo(){
+System.out.println("Jenis  \t:" + jenis+"\n"+
+                "Merk \t:"+merk+"\n"+
+                "Harga \t:"+harga);
+
 }
+}
+
