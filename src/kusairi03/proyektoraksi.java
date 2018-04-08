abef5f45f5cd650c64b6329e60cb5dea7eb7855c
kusairi <riskadelina96@gmail.com>
2018-04-08 21:45:56 +0800

    instance membuat atribut  dan memanggil method

--- src/kusairi03/proyektoraksi.java
+++ src/kusairi03/proyektoraksi.java
@@ -1,6 +1,12 @@
 package kusairi03;
 public class proyektoraksi {
     public static void main(String[] args) {
+        proyektor s = new proyektor();
+        s.jenis=("thosiba");
+        s.merk=("inside");
+        s.harga=100000;
+        
+        s.cetakInfo();
         
     }
 }
