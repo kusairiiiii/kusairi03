07a2923a108a3c85c803ece89f6ee63ed9ab9622
kusairi <riskadelina96@gmail.com>
2018-04-08 22:00:58 +0800

    atribut dan method

--- src/kusairi03/proyektorapple.java
+++ src/kusairi03/proyektorapple.java
@@ -1,4 +1,9 @@
 package kusairi03;
 public class proyektorapple extends proyektor {
     
+        private String ukuran;
+     
+    void throttle(){
+        System.out.println("string gaje.......;");
+    }    
 }
